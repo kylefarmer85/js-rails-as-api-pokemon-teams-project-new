8dc7f7d215956d9c94d28f48d355789c65c927df
kylefarmer85 <kylefarmer85@gmail.com>
2020-10-31 16:05:03 -0500

    FE properly fetching trainers from rails API

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,40 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector('main')
+
+function run() {
+  fetchTrainers()
+}
+
+
+
+function fetchTrainers() {
+  fetch('http://localhost:3000/trainers')
+  .then(resp => resp.json())
+  .then(trainers => {
+    trainers.forEach(renderTrainer)
+  });
+}
+
+function renderTrainer(trainer) {
+  main.innerHTML += 
+  `<div class="card" data-id=${trainer.id}><p>${trainer.name}</p>
+  <button data-trainer-id="${trainer.id}">Add Pokemon</button>
+  <ul></ul>
+  </div>`
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+run()
