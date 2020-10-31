fcfaff3a6b7a5999819e0cfe69b892f387efe8b1
kylefarmer85 <kylefarmer85@gmail.com>
2020-10-31 17:06:13 -0500

    trainers and pokemon rendering to dom

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -12,18 +12,31 @@ function run() {
 function fetchTrainers() {
   fetch('http://localhost:3000/trainers')
   .then(resp => resp.json())
-  .then(trainers => {
-    trainers.forEach(renderTrainer)
-  });
+  .then(trainers => trainers.forEach(renderTrainer)
+  )
 }
 
 function renderTrainer(trainer) {
   main.innerHTML += 
   `<div class="card" data-id=${trainer.id}><p>${trainer.name}</p>
-  <button data-trainer-id="${trainer.id}">Add Pokemon</button>
-  <ul></ul>
+    <button data-trainer-id="${trainer.id}">Add Pokemon</button>
+    <ul id=${trainer.id}>
+    </ul>
   </div>`
-} 
+
+  trainer.pokemons.forEach(renderPokemon)
+}
+
+function renderPokemon(pokemon) {
+  let ul = document.getElementById(pokemon.trainer_id)
+  let li = document.createElement('li')
+  li.innerHTML += `<li>${pokemon.nickname} (${pokemon.species})<button class="release" data-pokemon-id=${pokemon.id}>Release</button></li>`
+  ul.append(li)
+}
+
+function createPokemonListener() {
+  
+}
 
 
 
